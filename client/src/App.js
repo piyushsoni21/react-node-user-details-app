50a27563b7c392dda0b56a8c8837f042057120f1
Piyush Soni <piyushsoni421@gmail.com>
2025-08-07 15:50:05 +0530

    Add App component for User Details App

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Details App</h1>
+      <p>Welcome to the React frontend!</p>
+    </div>
+  );
+}
+
+export default App;
